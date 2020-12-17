83fba237599a16425d7b841fd0c9fb89457f36a7
RenanSGuedes <renanguedes.br@gmail.com>
2020-12-17 20:37:30 -0300

    :dog:

--- index.py
+++ index.py
@@ -1,31 +1,27 @@
-coord = [(-1, 4), (0, 1), (2, -1)]
+from sympy.abc import x
+from sympy import expand, simplify
 
+coord = [(1, 0), (3, 6), (4, 24), (5, 60)]
 deltas = [[] for _ in range(len(coord) - 1)]
-
-print("deltas:", deltas)
 
 for i in range (len(coord) - 1):
   deltas[0].append((coord[i + 1][1] - coord[i][1])/(coord[i + 1][0] - coord[i][0]))
-print("deltas:", deltas)
-
-
 
 for k in range(len(deltas) - 1):
   for i in range(len(deltas[k]) - 1):
     deltas[k + 1].append((deltas[k][i + 1] - deltas[k][i])/(coord[i + 2 + k][0] - coord[i][0]))
-  print("deltas:", deltas)
 
+firstItem = [item[0] for item in deltas]
+firstItem.insert(0, coord[0][1])
 
-'''
-for i in range(len(deltas[0]) - 1):
-  deltas[0 + 1].append((deltas[0][i + 1] - deltas[0][i])/(coord[i + 2][0] - coord[i][0]))
 print("deltas:", deltas)
+print(firstItem)
 
-for i in range(len(deltas[1]) - 1):
-  deltas[0 + 2].append((deltas[1][i + 1] - deltas[1][i])/(coord[i + 3][0] - coord[i][0]))
-print("deltas:", deltas)
+factors = []
 
-for i in range(len(deltas[2]) - 1):
-  deltas[0 + 3].append((deltas[2][i + 1] - deltas[2][i])/(coord[i + 4][0] - coord[i][0]))
-print("deltas:", deltas)
-'''
+for i in range(len(coord) - 1):
+  factors.append('(x - {})'.format(coord[i][0]))
+  
+print(factors)
+
+
