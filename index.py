a6061fdfee1a69ed9f49a4969a87691b01786f9c
RenanSGuedes <renanguedes.br@gmail.com>
2020-12-17 22:59:56 -0300

    :car:

--- index.py
+++ index.py
@@ -1,15 +1,26 @@
-from sympy.abc import x
-from sympy import expand, simplify
+from sympy.abc import x, y, _clash1
+from sympy.plotting import plot
+from sympy import *
+import matplotlib.pyplot as plt
+import numpy as np
 
-coord = [(1, 0), (3, 6), (4, 24), (5, 60)]
+coord = []
+
+print('Coordenadas (Duplo <enter> finaliza o input)')
+print('x y')
+i = input()
+while i != '':
+    coord.append([float(s) for s in i.split()])
+    i = input()
+
 deltas = [[] for _ in range(len(coord) - 1)]
 
 for i in range (len(coord) - 1):
-  deltas[0].append((coord[i + 1][1] - coord[i][1])/(coord[i + 1][0] - coord[i][0]))
+  deltas[0].append(round((coord[i + 1][1] - coord[i][1])/(coord[i + 1][0] - coord[i][0]), 3))
 
 for k in range(len(deltas) - 1):
   for i in range(len(deltas[k]) - 1):
-    deltas[k + 1].append((deltas[k][i + 1] - deltas[k][i])/(coord[i + 2 + k][0] - coord[i][0]))
+    deltas[k + 1].append(round((deltas[k][i + 1] - deltas[k][i])/(coord[i + 2 + k][0] - coord[i][0]), 3))
 
 firstItem = [item[0] for item in deltas]
 firstItem.insert(0, coord[0][1])
@@ -42,6 +53,25 @@ for i in range(1, len(newFactConc) + 1):
 
 print(finalFactors)
 
+parseResult = "{}+".format(coord[0][1]) + '+'.join(finalFactors)
 
-result = "{}+".format(coord[0][1]) + '+'.join(finalFactors)
-print(expand(result))
+result = sympify(parseResult, locals=_clash1)
+
+x_coord = [] # Armazena a posição de x para cada sublista de coord
+y_coord = [] # Armazena a posição de y para cada sublista de coord
+
+for i in range(len(coord)):
+  x_coord.append(coord[i][0])
+
+for i in range(len(coord)):
+  y_coord.append(coord[i][1])
+
+interval = np.linspace(coord[0][0], coord[-1][0], num=100)
+fx = []
+for i in range(len(interval)):
+  fx.append(result.subs(x, interval[i]))
+
+plt.plot(interval, fx, color='b')
+plt.grid(True, linewidth=.5, linestyle='--', color='r')
+plt.scatter(x_coord, y_coord, color='g')
+plt.show()
